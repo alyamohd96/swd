5da4ca095de592ad56ba6cefc289a3670ac91a23
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-08-30 11:33:34 -0500

    Created modules for oral exam 1

--- oral_exam1/S1_Computus_Easy/src/MainClass.java
+++ oral_exam1/S1_Computus_Easy/src/MainClass.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class MainClass {
 
     public static void main(String[] args)  {
+
+        System.out.println("Please enter year:");
+        Scanner year = new Scanner(System.in);
 
     }
 
