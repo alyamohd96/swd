fe61a92ddd2fdb284e4985ee4317c86f413f5519
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-08-30 11:26:19 -0500

    Started on homework computus

---
+++ oral_exam1/S1_Computus_Easy/src/EasterDate.java
@@ -0,0 +1,21 @@
+public class EasterDate {
+
+
+    public EasterDate() {
+    }
+
+    public EasterDate(int y) {
+        int year = y;
+    }
+
+
+    private EasterDate calculateEasterDate(int y)  {
+
+        int a = y % 19;
+        int b;
+        int c = y % 100;
+        int d;
+        int e;
+
+    }
+}
