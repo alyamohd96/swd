c48dec384777f55b432d3a4a14904ca7bd6c7143
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-09-11 11:46:32 -0500

    in class learning on 20171109

--- class_demos/ch08/fig08_07_09/src/Date.java
+++ class_demos/ch08/fig08_07_09/src/Date.java
@@ -16,10 +16,7 @@ public class Date {
                     "month (" + month + ") must be 1-12");
 
         // check if day in range for month
-        if (day <= 0 ||
-                (day > daysPerMonth[month] && !(month == 2 && day == 29)))
-            throw new IllegalArgumentException("day (" + day +
-                    ") out-of-range for the specified month and year");
+        checkDayInRange(day, month);
 
         // check for leap year if month is 2 and day is 29
         if (month == 2 && day == 29 && !(year % 400 == 0 ||
@@ -33,6 +30,50 @@ public class Date {
 
         System.out.printf(
                 "Date object constructor for date %s%n", this);
+    }
+
+    public static int[] getDaysPerMonth() {
+        return daysPerMonth;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        checkDayInRange(day, month);
+
+        this.day = day;
+    }
+
+    public void setDate(int month, int day, int year)   {
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
+
+    private static void checkDayInRange(int day, int month) {
+        // check if day in range for month
+        if (day <= 0 ||
+                (day > daysPerMonth[month] && !(month == 2 && day == 29)))
+            throw new IllegalArgumentException("day (" + day +
+                    ") out-of-range for the specified month and year");
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 
     // return a String of the form month/day/year
