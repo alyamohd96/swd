ae85483d7111c16c2d6a67a734ad89b0bcd2ed0f
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-09-18 20:22:02 -0500

    Created a new Date class. Generate getters and setters. Wrote the algorithm for calculating the easter date

--- oral_exam1/S1_Computus_Easy/src/EasterDate.java
+++ oral_exam1/S1_Computus_Easy/src/EasterDate.java
@@ -1,7 +1,11 @@
+import java.lang.*;
+
 public class EasterDate {
 
+    public Date easterDate;
 
-    public EasterDate() {
+    public EasterDate(Date easterDate) {
+        this.easterDate = easterDate;
     }
 
     public EasterDate(int y) {
@@ -11,11 +15,22 @@ public class EasterDate {
 
     private EasterDate calculateEasterDate(int y)  {
 
-        int a = y % 19;
-        int b;
-        int c = y % 100;
-        int d;
-        int e;
+        double a = y % 19;
+        double b = Math.floor(y/100);
+        System.out.println("b is" + b);
+        double c = y % 100;
+        double d = Math.floor(b/4);
+        double e = b % 4;
+        double f = Math.floor((b+8)/25);
+        double g = Math.floor((b-f+1)/3);
+        double h = ((19*a) + b - d - g + 15) % 30;
+        double i = Math.floor(c/4);
+        double k = c % 4;
+        double l = (32 + (2*e) + (2*i) - h - k) % 7;
+        double m = Math.floor((a+(11*h)+22l)/31);
+        int month = (int) Math.floor((h+l-(7*m)+114)/31);
+        System.out.println();
+        int day = (int) ((h+l-(7*m)+114) % 31) + 1;
 
     }
 }
