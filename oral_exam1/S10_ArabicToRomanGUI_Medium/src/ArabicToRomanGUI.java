376b3538f7e0ef10c8d98f6f00ada514084dd3d1
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-09-27 00:42:18 -0500

    Deleted old java docs. Added 2 classes for the ArabicToRoman homework.

---
+++ oral_exam1/S10_ArabicToRomanGUI_Medium/src/ArabicToRomanGUI.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ArabicToRomanGUI extends JFrame {
+
+    private final JLabel label1;
+    private final JLabel label2;
+    private final JTextField romanNumber;
+    private final JTextField arabicNumber;
+
+    public ArabicToRomanGUI()   {
+        super("Arabic To Roman");
+        setLayout(new FlowLayout());
+
+        label1 = new JLabel("Roman Number:");
+        add(label1);
+        romanNumber = new JTextField("Insert here",30);
+        add(romanNumber);
+
+        label2 = new JLabel("Arabic Number:");
+        add(label2);
+        arabicNumber = new JTextField(30);
+        add(arabicNumber);
+
+    }
+
+}
---
+++ oral_exam1/S10_ArabicToRomanGUI_Medium/src/ArabicToRomanMainClass.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class ArabicToRomanMainClass {
+
+    public static void main(String[] args)  {
+        ArabicToRomanGUI session1 = new ArabicToRomanGUI();
+        session1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        session1.setSize(300,400);
+        session1.setVisible(true);
+    }
+}
