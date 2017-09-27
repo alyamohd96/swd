3dc171aa2a61d08a8a8a1112cc7324e385ee99e2
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-09-26 22:33:28 -0500

    removed test codes

--- oral_exam1/S1_Computus_Medium/src/EasterDate.java
+++ oral_exam1/S1_Computus_Medium/src/EasterDate.java
@@ -10,7 +10,8 @@ import java.util.Arrays;
 
 public class EasterDate {
 
-    private Date easterDate;
+    //class fields
+    private Date easterDate;       //stores the easter date in Date type class
     private int[] dateArray;
     private int[] aprilCount = new int[26];
     private int[] marchCount = new int[10];
--- oral_exam1/S1_Computus_Medium/src/MainClass.java
+++ oral_exam1/S1_Computus_Medium/src/MainClass.java
@@ -5,8 +5,6 @@ public class MainClass {
 
     public static void main(String[] args)  {
 
-        Integer marchEasterDays[] = {22,23,24,25,26,27,28,29,30,31};
-
         System.out.println("Please enter year:");
         Scanner input = new Scanner(System.in);
         String year = input.next();
@@ -14,7 +12,6 @@ public class MainClass {
         EasterDate easterDate1 = new EasterDate(yearToCalculate);
         System.out.println(easterDate1.toString());
         easterDate1.calculateOneCycle();
-
 
     }
 
