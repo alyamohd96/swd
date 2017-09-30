e175c5eab2b3d4dd76e1dfed52614b29df964a2f
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-09-30 08:06:33 -0500

    Edit the GUI frame

--- oral_exam1/S10_ArabicToRomanGUI_Medium/src/ArabicToRomanGUI.java
+++ oral_exam1/S10_ArabicToRomanGUI_Medium/src/ArabicToRomanGUI.java
@@ -9,25 +9,33 @@ public class ArabicToRomanGUI extends JFrame {
     private final JLabel label2;
     private final JTextField romanNumber;
     private final JTextField arabicNumber;
+    private final Font defaultFont;
 
     public ArabicToRomanGUI()   {
         super("Arabic To Roman");
-        setLayout(new FlowLayout());
+        setLayout(new GridLayout(2,2));
+
+        defaultFont = new Font("Trebuchet MS", Font.PLAIN, 30);
 
         label1 = new JLabel("Roman Number:");
+        label1.setFont(defaultFont);
         add(label1);
         romanNumber = new JTextField("Insert here",30);
+        romanNumber.setFont(defaultFont);
         add(romanNumber);
 
         label2 = new JLabel("Arabic Number:");
+        label2.setFont(defaultFont);
         add(label2);
         arabicNumber = new JTextField(30);
+        arabicNumber.setFont(defaultFont);
         add(arabicNumber);
 
-        ArabicToRomanHandler handler = new ArabicToRomanHandler();
-        romanNumber.addActionListener(handler);
+        //ArabicToRomanHandler handler = new ArabicToRomanHandler();
+        //romanNumber.addActionListener(handler);
 
     }
+
 
     private int RomanToArabic(int romanNumber)  {
 
@@ -38,7 +46,7 @@ public class ArabicToRomanGUI extends JFrame {
         @Override
         public void actionPerformed(ActionEvent event)  {
             if(event.getSource() == romanNumber)
-                
+
         }
 
     }
