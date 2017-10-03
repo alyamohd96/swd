c9707f1172f42512fec943ac628d3ad6e0308d51
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-10-03 09:04:45 -0500

    completed the square class with calculateArea and calculatePerimeter methods

--- oral_exam1/S23_ShapeInheritanceHierarchy_Medium/src/Square.java
+++ oral_exam1/S23_ShapeInheritanceHierarchy_Medium/src/Square.java
@@ -12,6 +12,33 @@ public class Square extends TwoDimensionalShapes{
 
     public Square(String name, int dimX, int dimY) {
         super(name, dimX, dimY);
+        this.perimeter = calculatePerimeter(dimX);
+        this.area = calculateArea(dimX);
+    }
 
+    public int getPerimeter() {
+        return perimeter;
+    }
+
+    public void setPerimeter(int perimeter) {
+        this.perimeter = perimeter;
+        this.dimX = this.dimY = perimeter/4;
+    }
+
+    public int getArea() {
+        return area;
+    }
+
+    public void setArea(int area) {
+        this.area = area;
+        this.dimX = this.dimY = (int) Math.sqrt((double) area);
+    }
+
+    private static int calculatePerimeter(int dimX) {
+        return 4*dimX;
+    }
+
+    private static int calculateArea(int dimX)  {
+        return (int) Math.pow(dimX,2);
     }
 }
