4812989e4c18ed3e17cd04a472a91cd3b14a7a0c
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-10-03 11:09:02 -0500

    calculatePerimeter and calculateArea methods for the Triangle class

--- oral_exam1/S23_ShapeInheritanceHierarchy_Medium/src/Triangle.java
+++ oral_exam1/S23_ShapeInheritanceHierarchy_Medium/src/Triangle.java
@@ -13,20 +13,21 @@ public class Triangle extends TwoDimensionalShapes {
 
     public Triangle(String name, int dimX, int dimY)    {
         super(name, dimX, dimY);
-
     }
 
     public Triangle(String name, int dimX, int dimY, double sideA, double sideB)    {
         super(name, dimX, dimY);
         this.sideA = sideA;
         this.sideB = sideB;
+        this.perimeter = calculatePerimeter(dimX, sideA, sideB);
+        this.area = calculateArea(dimX, dimY);
     }
 
-    private static int calculatePerimeter(int dimX, int dimY) {
-
+    private static double calculatePerimeter(int dimX, double sideA, double sideB) {
+        return dimX + sideA + sideB;
     }
 
-    private static int calculateArea()  {
-
+    private static int calculateArea(int dimX, int dimY)  {
+        return (1/2) * dimX * dimY;
     }
 }
