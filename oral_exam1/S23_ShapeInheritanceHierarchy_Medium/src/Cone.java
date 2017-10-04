1b76871bbd8c6d9ba2d73ddb8ad11238d8e2616e
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-10-04 16:25:26 -0500

    added new cone subclass under threedimensionalshapes superclass

---
+++ oral_exam1/S23_ShapeInheritanceHierarchy_Medium/src/Cone.java
@@ -0,0 +1,33 @@
+public class Cone extends ThreeDimensionalShapes{
+
+    private double surfaceArea;
+    private double volume;
+
+    public Cone(String name)    {
+        super(name);
+    }
+
+    public Cone(String name, double radius, double height)  {
+        super(name, radius, radius, height);
+        this.surfaceArea = calculateSurfaceArea(radius,height);
+        this.volume = calculateVolume(radius, height);
+    }
+
+    public double getSurfaceArea() {
+        return surfaceArea;
+    }
+
+    public double getVolume() {
+        return volume;
+    }
+
+
+    private static double calculateSurfaceArea(double radius, double height)    {
+        double a = Math.pow(height,2) + Math.pow(radius,2);
+        return Math.PI*radius*(radius+(Math.sqrt(a)));
+    }
+
+    private static double calculateVolume(double radius, double height) {
+        return Math.PI*Math.pow(radius,2)*(height/3);
+    }
+}
