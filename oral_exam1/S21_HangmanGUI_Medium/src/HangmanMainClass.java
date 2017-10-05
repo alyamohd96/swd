c7fc94ce9c3e5fcfaa949dc14ecc5a34cf8369db
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-10-05 10:16:38 -0500

    deleted old tests module. created new tests module for hangman

--- oral_exam1/S21_HangmanGUI_Medium/src/HangmanMainClass.java
+++ oral_exam1/S21_HangmanGUI_Medium/src/HangmanMainClass.java
@@ -2,7 +2,7 @@ import javax.swing.JFrame;
 
 public class HangmanMainClass {
 
-    public static void main(String[] args)  {
+    //public static void main(String[] args)  {
         HangmanGUI session1 = new HangmanGUI();
         session1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         session1.setSize(800,1000);
