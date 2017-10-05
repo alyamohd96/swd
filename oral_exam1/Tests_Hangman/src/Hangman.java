12885864e4e2e82fd4a6a5e593874b624fe1e3cf
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-10-05 16:26:57 -0500

    added more methods for testing Hangman

--- oral_exam1/Tests_Hangman/src/Hangman.java
+++ oral_exam1/Tests_Hangman/src/Hangman.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
+/**
+ * http://www.cs.jhu.edu/~joanne/cs107/code/StaticMethods/hangmanMethods.java
+ */
+
 public class Hangman {
 
     private final int MAXPARTS = 6;
@@ -5,18 +11,75 @@ public class Hangman {
     private StringBuffer output;
     private int bodyparts;
     private boolean done;
-    private String guessWord;
-    private String lettersGuessed;
-    private char inputLetter;
+    private String guess;
+    private String guesses;
+    private char letter;
 
-    private char guessLetter;
+    private boolean win;
 
     public Hangman(String secret)    {
         this.secret = secret;
-        this.lettersGuessed = "";
+        this.guesses = "";
         this.bodyparts = MAXPARTS;
         this.done = false;
+        this.output = makeOutput(secret);
+        this.win = false;
+    }
 
+    public int getBodyparts() {
+        return bodyparts;
+    }
+
+
+    public void startHangman(String input)   {
+
+        this.guess = input;
+
+        if(this.guess.length() > 1)  {
+            if (this.guess.equals(secret))
+                System.out.print("You win");
+            else
+                System.out.print("You lose");
+
+            this.done = true;
+        }
+
+        else    {
+
+            letter = guess.charAt(0);
+            guesses += letter;
+
+            if(secret.indexOf(letter) < 0)
+                --bodyparts;
+            else
+                matchLetter(secret, output, letter);
+
+
+            System.out.print(getBodyparts());   //display body parts
+
+            if(isDone())    {
+                win = false;
+                done = true;
+            }
+            else if(secret.equals(output.toString())) {
+                win = true;
+                done = true;
+            }
+
+        }
+
+    }
+
+    private boolean isDone() {
+        return this.bodyparts == 0;
+    }
+
+    //when letter matched
+    private static void matchLetter(String secret, StringBuffer output, char letter)    {
+        for (int index = 0; index < secret.length(); index++)
+            if (secret.charAt(index) == letter)
+                output.setCharAt(index, letter);
+        System.out.print("good guess - ");
 
     }
 
@@ -27,5 +90,22 @@ public class Hangman {
         return output;
     }
 
+
+    public static void main(String args[])  {
+        System.out.print("Enter secret word:");
+        Scanner input = new Scanner(System.in);
+        String secretWord = input.next();
+
+        Hangman session1 = new Hangman(secretWord);
+        while(! session1.done) {
+            System.out.print("enter letter/word:");
+            session1.startHangman(input.next());
+
+        }
+
+        session1.win = true;
+
+
+    }
 
 }
