8ed63774930f23ab0fd8ba46bb5322aec8e9b8f4
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-10-05 10:53:00 -0500

    created a java test class for hangman

--- oral_exam1/Tests_Hangman/src/Hangman.java
+++ oral_exam1/Tests_Hangman/src/Hangman.java
@@ -1,2 +1,31 @@
 public class Hangman {
+
+    private final int MAXPARTS = 6;
+    private String secret;
+    private StringBuffer output;
+    private int bodyparts;
+    private boolean done;
+    private String guessWord;
+    private String lettersGuessed;
+    private char inputLetter;
+
+    private char guessLetter;
+
+    public Hangman(String secret)    {
+        this.secret = secret;
+        this.lettersGuessed = "";
+        this.bodyparts = MAXPARTS;
+        this.done = false;
+
+
+    }
+
+    private static StringBuffer makeOutput(String secret)    {
+        StringBuffer output = new StringBuffer(secret.length());
+        for(int count=0; count < secret.length(); count++)
+            output.append('-');
+        return output;
+    }
+
+
 }
