b58ef6230c5db465ef73d3c93eff12416bb71001
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-10-06 19:44:10 -0500

    generated javadocs for hangman module

--- oral_exam1/S21_HangmanGUI_Hard/src/HangmanGUI.java
+++ oral_exam1/S21_HangmanGUI_Hard/src/HangmanGUI.java
@@ -5,30 +5,77 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * This HangmanGUI class is the class
+ * This HangmanGUI class is the class that creates the game's interface
+ * This class extends JFrame class and has an anonymous inner class and
+ * an inner class for the action event handling component.
  */
-
 public class HangmanGUI extends JFrame {
 
-    //instance variables for the GUI
+    /**
+     * label1 is the Welcome to Hangman! label
+     */
     private final JLabel label1;
+    /**
+     * label2 is the Insert secret word:
+     */
     private final JLabel label2;
+    /**
+     * secretWord is where the user will input the secret word that will guessed
+     */
     private final JPasswordField secretWord;
+    /**
+     * label3 is the Enter your guess here: label
+     */
     private final JLabel label3;
+    /**
+     * textfield1 is the text field where the user will input their guess
+     */
     private final JTextField textField1;
+    /**
+     * label4 is the Number of guesses left: label
+     */
     private final JLabel label4;
+    /**
+     * textfield2 will display the number of guesses left by the user
+     */
     private final JTextField textField2;
+    /**
+     * defaultFont is the font for all the components in the JFrame.
+     * The variable will be set to Trebuchet MS if size 30
+     */
     private final Font defaultFont;
+    /**
+     * label5 is the Secret word will be displayed here label
+     */
     private final JLabel label5;
+    /**
+     * label6 is the Letters guessed: label
+     */
     private final JLabel label6;
+    /**
+     * label7 will show the letters guessed by the user
+     */
     private final JLabel label7;
 
+    /**
+     * hangmanFigure is an instance variable of type HangmanFigure
+     * that generates the hangman figure
+     */
     private final HangmanFigure hangmanFigure;
 
-    //
+    /**
+     * hangman1 is an instance variable of type Hangman that
+     * contains the basic methods for the hangman game
+     */
     private Hangman hangman1;
 
 
+    /**
+     * This no argument constructor for this class will instantiate the
+     * instance variables and add them to the JFrame. The layout of the
+     * JFrame is the GridLayout of 5 rows and 5 columns This constructor
+     * will also instantiate a action handler for the textfields and labels
+     */
     public HangmanGUI(){
         super("Hangman");
         setLayout(new GridLayout(5,5));    //set frame layout
@@ -58,8 +105,10 @@ public class HangmanGUI extends JFrame {
         label5.setFont(new Font("Trebuchet MS", Font.BOLD, 36));
         add(label5);
 
+        //empty JLabel added to the JFrame to leave one grid empty
         add(new JLabel(""));
 
+        //instantiate the hangman figure
         hangmanFigure = new HangmanFigure();
         hangmanFigure.setSize(600,1000);
         hangmanFigure.setAlignmentX(CENTER_ALIGNMENT);
@@ -75,6 +124,7 @@ public class HangmanGUI extends JFrame {
         secretWord.setFont(defaultFont);
         add(secretWord);
 
+        //empty JLabel added to the JFrame to leave one grid empty
         add(new JLabel(""));
 
         //Enter your guess here Label
@@ -88,6 +138,7 @@ public class HangmanGUI extends JFrame {
         textField1.setFont(defaultFont);
         add(textField1);
 
+        //empty JLabel added to the JFrame to leave one grid empty
         add(new JLabel(""));
 
         label6.setHorizontalAlignment(SwingConstants.LEFT);
@@ -117,8 +168,17 @@ public class HangmanGUI extends JFrame {
         secretWord.addActionListener(
                 //anonymous inner class for secretWord JTextField
                 new ActionListener() {
+
+                    /**
+                     * This anonymous inner class for secretWord makes the textfield1 uneditable
+                     * after the secret word has entered and instantiates the hangman game. it
+                     * will also changes the input from the user to uppercase for simplicity
+                     * @param e
+                     */
                     @Override
                     public void actionPerformed(ActionEvent e) {
+
+                        //instantiate the hangman object and checks the input for validity
                         secretWord.setEditable(false);
                         String secret = secretWord.getText().toUpperCase();
                         if(!secret.isEmpty() || isAlpha(secret)) {
@@ -132,6 +192,8 @@ public class HangmanGUI extends JFrame {
                     }
                 }
         );
+
+        //instantiate a new HangmanHandler and binds it to textfield1
         HangmanHandler handler = new HangmanHandler();
         textField1.addActionListener(handler);
     }
@@ -156,8 +218,21 @@ public class HangmanGUI extends JFrame {
     }
 
 
+    /**
+     * this private inner class is the class that handles any action
+     * event performed by textfield1. it implements the ActionListener class
+     * and Overrides the actionPerformed methods.
+     */
     private class HangmanHandler implements ActionListener {
 
+        /**
+         * This method is the overriden method from ActionListener.
+         * When an action event occurs from textfield1, the method
+         * will call the startHangman method from the Hangman object, hangman1.
+         * When the games ends, a JOptionPane window will be displayed indicating
+         * the player whether they have lost or won
+         * @param e
+         */
         @Override
         public void actionPerformed(ActionEvent e) {
 
--- oral_exam1/S21_HangmanGUI_Hard/src/HangmanMainClass.java
+++ oral_exam1/S21_HangmanGUI_Hard/src/HangmanMainClass.java
@@ -1,5 +1,11 @@
 import javax.swing.JFrame;
 
+/**
+ * This is the driver class for the HangmanGUI. It instantiates
+ * the HangmanGUI and set the settings of the JFrame. The
+ * close button on the JFrame will exit when the close button
+ * is pressed. It will also set the size of JFrame to 2000X2000
+ */
 public class HangmanMainClass {
 
     public static void main(String[] args)  {
