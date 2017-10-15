d854c0612927f2e70a5e63bac4419971232cc326
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-10-15 10:06:56 -0500

    Created new modules and created instance variables of Buttons

---
+++ oral_exam2/12-9_Calculator_Hard/src/CalculatorGUI.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+
+public class CalculatorGUI extends JFrame {
+
+    private final JTextField input;
+
+    private final JButton num1;
+    private final JButton num2;
+    private final JButton num3;
+    private final JButton num4;
+    private final JButton num5;
+    private final JButton num6;
+    private final JButton num7;
+    private final JButton num8;
+    private final JButton num9;
+    private final JButton num0;
+
+    private final JButton divide;
+    private final JButton multiply;
+    private final JButton subtract;
+    private final JButton add;
+    private final JButton equal;
+    private final JButton decimal;
+
+
+
+}
