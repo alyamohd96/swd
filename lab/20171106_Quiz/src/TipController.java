ce31af54b50b8bb84a35b0fbf32bd1086e7f6b88
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-06 12:19:15 -0600

    quiz for nov 6 - controller class

--- lab/20171106_Quiz/src/TipController.java
+++ lab/20171106_Quiz/src/TipController.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
+import java.math.BigDecimal;
+import javafx.event.ActionEvent;
+
 
 public class TipController {
 
@@ -22,6 +25,22 @@ public class TipController {
 
     @FXML
     private TextField totalTextField;
+
+    @FXML
+    private void calculator(ActionEvent event)  {
+        try {
+            BigDecimal amount = new BigDecimal(amountTextField.getText());
+            BigDecimal tip = amount.multiply(tipPercentage);
+            BigDecimal total = amount.add(tip);
+
+            tipTextField.setText(currency.format(tip));
+            totalTextField.setText(currency.format(total));
+        } catch (NumberFormatException ex) {
+            amountTextField.setText("Enter amount");
+            amountTextField.selectAll();
+            amountTextField.requestFocus();
+        }
+    }
 
 }
 
