37934f10888a1a60cfc4171b2cfe65566544aee6
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-06 12:07:34 -0600

    quiz for nov 6

---
+++ lab/20171106_Quiz/src/TipCalculator.java
@@ -0,0 +1,18 @@
+/**
+ * Created by ttmohd on 11/6/17.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TipCalculator extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ lab/20171106_Quiz/src/TipController.java
@@ -0,0 +1,27 @@
+/**
+ * Created by ttmohd on 11/6/17.
+ */
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+
+public class TipController {
+
+    @FXML
+    private Slider tipPercentageSlider;
+
+    @FXML
+    private Label tipPercentageLabel;
+
+    @FXML
+    private TextField tipTextField;
+
+    @FXML
+    private TextField amountTextField;
+
+    @FXML
+    private TextField totalTextField;
+
+}
+
