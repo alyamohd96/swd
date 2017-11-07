5cd760ece717588636550ae20f9d9b8ff9a51b3a
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-07 10:56:34 -0600

    added calculateArea method and getArea method

--- oral_exam2/10-13_Shapes_Easy/src/Circle.java
+++ oral_exam2/10-13_Shapes_Easy/src/Circle.java
@@ -1,2 +1,24 @@
 public class Circle {
+
+    private double radius;
+
+    public Circle() {
+
+    }
+
+    @Override
+    public double getArea() {
+        return calculateArea(radius);
+    }
+
+    /**
+     * A static method for calculating an area of the circle given
+     * a radius
+     * @param radius radius of a circle
+     * @return area of the circle
+     */
+    public static double calculateArea(double radius)   {
+        return Math.PI * Math.pow(radius,2);
+    }
+
 }
