67788e67ab41d5ec0bfa193c732be3c09883206a
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-13 12:10:21 -0600

    added processing number pad function

--- lab/20171110_Quiz/src/CalculatorController.java
+++ lab/20171110_Quiz/src/CalculatorController.java
@@ -10,79 +10,17 @@ public class CalculatorController {
 
 
     @FXML
-    private Button sevenButton;
-
-    @FXML
-    private Button eightButton;
-
-    @FXML
-    private Button nineButton;
-
-    @FXML
-    private Button divideButton;
-
-    @FXML
-    private Button squareButton;
-
-    @FXML
-    private Button fourButton;
-
-    @FXML
-    private Button fiveButton;
-
-    @FXML
-    private Button sixButton;
-
-    @FXML
-    private Button multiplyButton;
-
-    @FXML
-    private Button sinButton;
-
-    @FXML
-    private Button oneButton;
-
-    @FXML
-    private Button twoButton;
-
-    @FXML
-    private Button threeButton;
-
-    @FXML
-    private Button minusButton;
-
-    @FXML
-    private Button cosButton;
-
-    @FXML
-    private Button zeroButton;
-
-    @FXML
-    private Button clearButton;
-
-    @FXML
-    private Button equalsButton;
-
-    @FXML
-    private Button addButton;
-
-    @FXML
-    private Button expButton;
-
-    @FXML
     private TextField displayTextField;
 
     @FXML
     private void processNumber(ActionEvent event) {
         if (start) {
-            output.setText("");
+            displayTextField.setText("");
             start = false;
         }
-
         String value = ((Button)event.getSource()).getText();
-        output.setText(output.getText() + value);
+        displayTextField.setText(displayTextField.getText() + value);
     }
-
 
 
 
