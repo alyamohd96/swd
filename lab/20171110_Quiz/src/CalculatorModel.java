47ddf49214d5c95aea72c7167a9b440a8360211b
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-13 11:48:37 -0600

    added calculate function in Model

--- lab/20171110_Quiz/src/CalculatorModel.java
+++ lab/20171110_Quiz/src/CalculatorModel.java
@@ -4,6 +4,29 @@ public class CalculatorModel {
     private double num2;
     private String operator;
 
-    public
+    public CalculatorModel(double num1, double num2, String operator)    {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.operator = operator;
+    }
+
+    public double calculate()   {
+        switch (operator) {
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                if (num2 == 0)
+                    return 0;
+
+                return num1 / num2;
+        }
+
+        System.out.println("Unknown operator - " + operator);
+        return 0;
+    }
 
 }
