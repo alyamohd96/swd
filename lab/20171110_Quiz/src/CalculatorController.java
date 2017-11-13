5d26e9d08a32b4fccc8796a9d0c01c65d57e5a77
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-13 11:40:37 -0600

    controller skeleton code

--- lab/20171110_Quiz/src/CalculatorController.java
+++ lab/20171110_Quiz/src/CalculatorController.java
@@ -1,2 +1,70 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CalculatorController {
+
+    @FXML
+    private Button sevenButton;
+
+    @FXML
+    private Button eightButton;
+
+    @FXML
+    private Button nineButton;
+
+    @FXML
+    private Button divideButton;
+
+    @FXML
+    private Button squareButton;
+
+    @FXML
+    private Button fourButton;
+
+    @FXML
+    private Button fiveButton;
+
+    @FXML
+    private Button sixButton;
+
+    @FXML
+    private Button multiplyButton;
+
+    @FXML
+    private Button sinButton;
+
+    @FXML
+    private Button oneButton;
+
+    @FXML
+    private Button twoButton;
+
+    @FXML
+    private Button threeButton;
+
+    @FXML
+    private Button minusButton;
+
+    @FXML
+    private Button cosButton;
+
+    @FXML
+    private Button zeroButton;
+
+    @FXML
+    private Button clearButton;
+
+    @FXML
+    private Button equalsButton;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button expButton;
+
+    @FXML
+    private TextField displayTextField;
+
 }
