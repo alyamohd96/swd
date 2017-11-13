5329cda6112f1a942944819593e6e43fa9e273f2
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-13 11:51:42 -0600

    added result String in model

--- lab/20171110_Quiz/src/CalculatorModel.java
+++ lab/20171110_Quiz/src/CalculatorModel.java
@@ -3,11 +3,13 @@ public class CalculatorModel {
     private double num1;
     private double num2;
     private String operator;
+    private String result;
 
     public CalculatorModel(double num1, double num2, String operator)    {
         this.num1 = num1;
         this.num2 = num2;
         this.operator = operator;
+        this.result = "0";
     }
 
     public double calculate()   {
@@ -19,10 +21,13 @@ public class CalculatorModel {
             case "*":
                 return num1 * num2;
             case "/":
-                if (num2 == 0)
+                if (num2 == 0) {
+                    result = "Cannot be divided by 0";
                     return 0;
-
+                }
+                else    {
                 return num1 / num2;
+                }
         }
 
         System.out.println("Unknown operator - " + operator);
