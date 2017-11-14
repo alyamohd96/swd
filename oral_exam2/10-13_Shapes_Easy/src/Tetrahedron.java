2abcdae09270421f7253f942f3ff70b91bdd93f2
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-14 11:52:32 -0600

    created new driver class and added implementation for triangle and tetrahedron

--- oral_exam2/10-13_Shapes_Easy/src/Tetrahedron.java
+++ oral_exam2/10-13_Shapes_Easy/src/Tetrahedron.java
@@ -1,2 +1,26 @@
-public class Tetrahedron {
+public class Tetrahedron extends ThreeDimensionalShape {
+
+    private double edge;
+
+    public Tetrahedron(double edge) {
+        this.edge = edge;
+    }
+
+    @Override
+    public double getSurfaceArea() {
+        return calculateSurfaceArea(this.edge);
+    }
+
+    @Override
+    public double getVolume()   {
+        return calculateVolume(this.edge);
+    }
+
+    public static double calculateSurfaceArea(double edge)    {
+        return Math.sqrt(3)*Math.pow(edge,2);
+    }
+
+    public static double calculateVolume(double edge)  {
+        return Math.pow(edge,3) / (6*Math.sqrt(2));
+    }
 }
--- oral_exam2/10-13_Shapes_Easy/src/Triangle.java
+++ oral_exam2/10-13_Shapes_Easy/src/Triangle.java
@@ -1,2 +1,35 @@
-public class Triangle {
+/**
+ * This Triangle class is a concrete class that inherits from TwoDimensionalShapes
+ * and indirectly implements the ShapesBaseClass. It contains 2 instance variables, width and height, and
+ * a 2 argument constructor that takes in width and height of the Triangle.
+ */
+public class Triangle extends TwoDimensionalShapes {
+
+    private double width;
+    private double height;
+
+    public Triangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    /**
+     *
+     * @return
+     */
+    @Override
+    public double getArea() {
+        return calculateArea(this.width, this.height);
+    }
+
+    /**
+     * This is a public static method to calculate an area of a triangle.
+     * @param width width of triangle
+     * @param height height of triangle
+     * @return area of triangle
+     */
+    public static double calculateArea(double width, double height) {
+        return (0.5)*(width)*(height);
+    }
+
 }
