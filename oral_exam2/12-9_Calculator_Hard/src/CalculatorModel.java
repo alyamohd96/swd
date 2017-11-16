d8fd37dd8885cf6fb869ea142f2f2899870f5f20
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-16 11:45:25 -0600

    edited calculator model

--- oral_exam2/12-9_Calculator_Hard/src/CalculatorModel.java
+++ oral_exam2/12-9_Calculator_Hard/src/CalculatorModel.java
@@ -1,7 +1,32 @@
+//TODO have to check how this is going to work because it is not working for now.
 public class CalculatorModel {
 
+    private double number1;
+    private double number2;
+    //private String operator;
 
-    public double calculate(double number1, double number2, String operator)  {
+    /*public CalculatorModel(double number1, double number2) {
+        this.number1 = number1;
+        this.number2 = number2;
+    }*/
+
+    public double getNumber1() {
+        return number1;
+    }
+
+    public void setNumber1(double number1) {
+        this.number1 = number1;
+    }
+
+    public double getNumber2() {
+        return number2;
+    }
+
+    public void setNumber2(double number2) {
+        this.number2 = number2;
+    }
+
+    public static double calculate(double number1, double number2, String operator)  {
         switch (operator) {
             case "+":
                 return number1 + number2;
