5765714cc47372532ec65495d4f0cc5ffc8ddea5
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-18 22:33:44 -0600

    constructor and methods for Converter class

--- oral_exam2/14-23_MetricConversion_Medium/src/Converter.java
+++ oral_exam2/14-23_MetricConversion_Medium/src/Converter.java
@@ -3,9 +3,16 @@ public class Converter {
     private String fromUnit;
     private String toUnit;
     private double fromValue;
-    private double toValue;
 
-    private String
+    public Converter(String fromUnit, String toUnit, double fromValue) {
+        this.fromUnit = fromUnit;
+        this.toUnit = toUnit;
+        this.fromValue = fromValue;
+    }
+
+    private String measurement()    {
+
+    }
 
     public void convert()   {
 
