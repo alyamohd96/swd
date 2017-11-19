9e76f5f41d94c7d0b66f3038a4afea0ed3253e55
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-18 22:20:44 -0600

    created 2 classes for Metric Conversion

---
+++ oral_exam2/14-23_MetricConversion_Medium/src/Converter.java
@@ -0,0 +1,13 @@
+public class Converter {
+
+    private String fromUnit;
+    private String toUnit;
+    private double fromValue;
+    private double toValue;
+
+    private String
+
+    public void convert()   {
+
+    }
+}
