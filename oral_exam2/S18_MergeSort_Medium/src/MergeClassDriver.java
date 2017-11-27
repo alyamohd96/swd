6b480f68b655177343ca8a4297e011adbfd11bc9
Alya Mohd <tengku-tmohd@uiowa.edu>
2017-11-26 18:23:39 -0600

    made the array sorted when a MergeSort object is created

--- oral_exam2/S18_MergeSort_Medium/src/MergeClassDriver.java
+++ oral_exam2/S18_MergeSort_Medium/src/MergeClassDriver.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 
 public class MergeClassDriver {
@@ -5,14 +6,14 @@ public class MergeClassDriver {
     public static void main(String args[])  {
 
         Random rand = new Random();
-        int[] numberArray = new int[100];
+        final int[] numberArray = new int[100];
 
-        for(int num:numberArray)    {
+        for(int num = 0; num<numberArray.length; num++)    {
             numberArray[num] = rand.nextInt(100);
         }
+        System.out.println(Arrays.toString(numberArray));
 
-
-
+        MergeSort mergeArray = new MergeSort(numberArray, 0, numberArray.length-1);
+        System.out.println(Arrays.toString(numberArray));
     }
-
 }
